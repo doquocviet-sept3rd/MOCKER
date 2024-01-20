b593527525abd7fb112fb2b328adb436936f6725
Do Quoc Viet <doquocviet.sept3rd@gmail.com>
2024-01-20 10:34:15 +0700

    Fix typo

--- FRONTEND/src/app/component/home/component/home/home.component.ts
+++ FRONTEND/src/app/component/home/component/home/home.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-hone',
-  templateUrl: 'hone.component.html',
+  selector: 'app-home',
+  templateUrl: 'home.component.html',
   styleUrls: ['home.component.scss']
 })
 export class HomeComponent {
